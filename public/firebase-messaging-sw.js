0dba0b4f10fae1266e35cdc24d6c87e30ab65dd1
YScott Domes <scott@musefind.com>
2017-07-20 07:40:37 -0700

    Added SW

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,42 +1,18 @@
-// Name our cache
-var CACHE_NAME = 'my-pwa-cache-v2';
+ importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+ importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 
-// Delete old caches that are not our current one!
+ firebase.initializeApp({
+   'messagingSenderId': '85734589405'
+ });
+
+self.addEventListener('install', function() {
+  console.log('Install!');
+});
+
 self.addEventListener("activate", event => {
-  console.log('activate')
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys()
-      .then(keyList =>
-        Promise.all(keyList.map(key => {
-          if (!cacheWhitelist.includes(key)) {
-            console.log('Deleting cache: ' + key)
-            return caches.delete(key);
-          }
-        }))
-      )
-  );
+  console.log('Activate!')
 });
 
-
-// The first time the user starts up the PWA, 'install' is triggered.
-self.addEventListener('install', function(event) {
-  console.log('install')
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        cache.addAll(['/', 'manifest.json'])
-      })
-  );
-});
-
-// When the webpage goes to fetch files, we intercept that request and serve up the matching files
-// if we have them
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-      caches.match(event.request).then(function(response) {
-          console.log(event.request, 'cache attempt')
-          return response || fetch(event.request);
-      })
-  );
+  console.log('Fetch!', event.request);
 });
