b2e4c58fd8a6ae10cc9ede7431d2b4774289761b
YScott Domes <scott@musefind.com>
2017-08-12 20:40:46 -0700

    Done caching

--- build/firebase-messaging-sw.js
+++ build/firebase-messaging-sw.js
@@ -1,18 +1,60 @@
- importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
- importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 
- firebase.initializeApp({
-   'messagingSenderId': '85734589405'
- });
+firebase.initializeApp({
+  messagingSenderId: '85734589405'
+});
 const messaging = firebase.messaging();
 
-self.addEventListener('install', function() {
-  console.log('Install!');
+messaging.setBackgroundMessageHandler(function(payload) {
+  // console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // // Customize notification here
+  // const notificationTitle = 'Background Message Title';
+  // const notificationOptions = {
+  //   body: 'Background Message body.',
+  //   icon: '/icon.png'
+  // };
+  // return self.registration.showNotification(notificationTitle,
+  //     notificationOptions);
 });
 
-self.addEventListener("activate", event => {
-  console.log('Activate!')
+const CACHE_NAME = 'v1';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      fetch('asset-manifest.json')
+        .then(response => {
+          if (response.ok) {
+            response.json().then(manifest => {
+              const urls = Object.keys(manifest).map(key => manifest[key]);
+              urls.push('/');
+              cache.addAll(urls);
+            });
+          }
+        })
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,12 +1,12 @@
- importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
- importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 
- firebase.initializeApp({
-   'messagingSenderId': '85734589405'
- });
-  const messaging = firebase.messaging();
+firebase.initializeApp({
+  messagingSenderId: '85734589405'
+});
+const messaging = firebase.messaging();
 
- messaging.setBackgroundMessageHandler(function(payload) {
+messaging.setBackgroundMessageHandler(function(payload) {
   // console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // // Customize notification here
   // const notificationTitle = 'Background Message Title';
@@ -18,13 +18,43 @@
   //     notificationOptions);
 });
 
-self.addEventListener('install', function() {
-  console.log('Install!');
+const CACHE_NAME = 'v1';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      fetch('asset-manifest.json')
+        .then(response => {
+          if (response.ok) {
+            response.json().then(manifest => {
+              const urls = Object.keys(manifest).map(key => manifest[key]);
+              urls.push('/');
+              cache.addAll(urls);
+            });
+          }
+        })
+    })
+  );
 });
 
-self.addEventListener("activate", event => {
-  console.log('Activate!')
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
